2af7f17181177e92f3c4ae0b3de4139b9b0bc126
Ayman Omer <aymangebrilomer@gmail.com>
2021-05-20 05:12:29 -0400

    newupdate

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
+dotenv.config();
 let MONGODB_URI =
   process.env.PROD_MONGODB ||
   process.env.MONGODB_URI ||
-  "mongodb://127.0.0.1:27017/dealershipDatabase";
+  process.env.MONGODB_URL;
 
 mongoose
   .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
