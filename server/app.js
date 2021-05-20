5af87ed1cab3c759034414b55751a96ead26c35e
Ayman Omer <aymangebrilomer@gmail.com>
2021-05-20 05:17:24 -0400

    add new update

--- server/app.js
+++ server/app.js
@@ -16,6 +16,15 @@ app.use(
 app.use(logger("dev"));
 const PORT = process.env.PORT || 5000;
 app.use("/api", router);
+// --> Add this
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
