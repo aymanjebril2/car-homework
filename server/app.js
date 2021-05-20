43c1acf3124784be95235964e83758b79d8fb101
Ayman Omer <aymangebrilomer@gmail.com>
2021-05-20 06:05:52 -0400

    new update

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use(cors(corsOptions));
+
 const whitelist = [
   "http://localhost:5000",
   "https://carbrands-name.herokuapp.com",
