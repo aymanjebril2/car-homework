71c130d77e42ee53ca44ee80cf31408f4563fbb4
Ayman Omer <aymangebrilomer@gmail.com>
2021-05-20 12:37:22 -0400

    home work

--- server/app.js
+++ server/app.js
@@ -5,53 +5,18 @@ import router from "./routes/index.js";
 import db from "./db/index.js";
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+app.use(cors());
 
-// in production on Heroku - re-route everything to https
-if (process.env.NODE_ENV === "production") {
-  app.use((req, res, next) => {
-    if (req.header("x-forwarded-proto") !== "https") {
-      res.redirect("https://" + req.hostname + req.url);
-    } else {
-      next();
-    }
-  });
-}
+const PORT = process.env.PORT || 8080;
+app.use(express.static("public"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-function removePortIfDev(url) {
-  const portRegex = /:[0-9]*$/;
-  return url.replace(portRegex, "");
-}
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      /*
-       * Get rid of port number in dev environment to allow other local apps through. Also, sometimes the origin
-       * comes in as the string "null"
-       */
-      const originToCheck = removePortIfDev(
-        !origin || origin === "null" ? "" : origin
-      );
-      const allowlist = [
-        "http://localhost", //...(process.env.NODE_ENV !== "production" ? [ "http://localhost" ] : []),
-        "https://cars-brands.herokuapp.com",
-      ];
-
-      if (!originToCheck || allowlist.includes(originToCheck)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    optionsSuccessStatus: 200,
-  })
-);
 
 app.use(logger("dev"));
 
 app.use("/api", router);
+
 // --> Add this
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
